975d7e24bf5772ae0e445d0cc3178d691878abe6
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-02 21:56:17 +0800

    Add allowedFields

---
+++ @utils/allowedFields.ts
@@ -0,0 +1,14 @@
+const repairFields = [
+  "unit_id",
+  "serial_number",
+  "unit_findings",
+  "actual_problem",
+  "work_done",
+  "date_returned",
+  "date_repaired",
+  "unit_status",
+  "unit_remarks",
+  "unit_category",
+];
+
+export { repairFields };
