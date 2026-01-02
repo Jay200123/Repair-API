a3276df333b8ce93b3bf04efc0949f9bf26ffcf3
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-02 21:55:18 +0800

    Add RepairService class layer for handling business logic related to repair details

---
+++ @services/repairService.ts
@@ -0,0 +1,43 @@
+import { STATUSCODE } from "../@constants";
+import { RepairRepository } from "../@repositories";
+import { ErrorHandler, repairFields, checkFields } from "../@utils";
+import { RepairDetails } from "../@types";
+
+export class RepairService {
+  constructor(private repairRepository: RepairRepository) {}
+
+  async getAllRepairs() {
+    const results = await this.repairRepository.getAll();
+
+    if (!results.length) {
+      throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Repairs not found");
+    }
+
+    return results;
+  }
+
+  async getRepairById(id: number) {
+    const result = await this.repairRepository.getById(id);
+
+    if (!result.length) {
+      throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Repair details not found");
+    }
+
+    return result;
+  }
+
+  async createRepair(data: RepairDetails) {
+    // Enforce allowed payload fields
+    checkFields(repairFields, data);
+
+    const result = await this.repairRepository.create(data);
+
+    const isCreated = result?.affectedRows == 1 ? true : false;
+
+    return isCreated
+      ? "Repair Details created successfully"
+      : "Invalid Request";
+  }
+
+  async updateRepairById() {}
+}
