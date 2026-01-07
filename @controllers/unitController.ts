653a1217d64df0e3ddd76c1f08d35c776f8e66af
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-07 23:31:59 +0800

    Add getAllUnits Request Reponse layer

--- @controllers/unitController.ts
+++ @controllers/unitController.ts
@@ -1,1 +1,26 @@
-export class UnitController {}
+import { STATUSCODE } from "../@constants";
+import { UnitService } from "../@services/unitService";
+import { MiddlewareFn } from "../@types";
+import { logger, SuccessHandler } from "../@utils";
+
+export class UnitController {
+  constructor(private unitService: UnitService) {}
+
+  getAllUnits: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_UNITS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const results = await this.unitService.getAllUnits();
+
+    logger.info({
+      GET_ALL_UNITS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, results, "Success");
+  };
+}
