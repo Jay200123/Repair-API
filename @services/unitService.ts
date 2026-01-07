6598545893905eba43cc2706a3f820fc3d4031d6
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-07 23:33:03 +0800

    Add `getAll` async method `units` process logic

--- @services/unitService.ts
+++ @services/unitService.ts
@@ -1,1 +1,17 @@
-export class UnitService {}
+import { STATUSCODE } from "../@constants";
+import { UnitRepository } from "../@repositories/unitRepository";
+import { ErrorHandler } from "../@utils";
+
+export class UnitService {
+  constructor(private unitRepository: UnitRepository) {}
+
+  async getAllUnits() {
+    const results = await this.unitRepository.getAll();
+
+    if (results.length == 0) {
+      throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Units not Found");
+    }
+
+    return results;
+  }
+}
