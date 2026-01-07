82eeb8ebe4bc26477d1b631c6195cec51a30cc0e
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-07 23:31:48 +0800

    ADD `units` path constant

--- @constants/path.ts
+++ @constants/path.ts
@@ -3,4 +3,7 @@ export const PATH = {
   REPAIR_ID: "/repair/:id",
   EDIT_REPAIR_ID: "/edit/repair/:id",
   REPAIRS_REPAIRED: "/repairs/repaired",
+  UNITS: "/units",
+  UNIT_ID: "/unit/:id",
+  EDIT_UNIT_ID: "/edit/unit/:id",
 };
