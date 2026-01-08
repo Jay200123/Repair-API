d43be3902508a32abfa1060f7f9dc811c6223772
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-08 23:59:07 +0800

    Add route endpoints & OOP dependency injection related on Unit class service controller & repository layers

--- @routes/unit.ts
+++ @routes/unit.ts
@@ -1,5 +1,42 @@
 import express from "express";
+import { Database } from "../@config";
+import { UnitRepository, SettingsRepository } from "../@repositories";
+import { UnitService } from "../@services";
+import { UnitController } from "../@controllers";
+import { PATH } from "../@constants";
+import { getUnitsBySkuQueryParams } from "./../@validations";
+import { AuthenticationMiddleware } from "../@middlewares";
 
 const router = express.Router();
+
+const database = new Database();
+const unitRepository = new UnitRepository(database);
+
+const unitService = new UnitService(unitRepository);
+
+const unitController = new UnitController(unitService);
+
+const settingsRepository = new SettingsRepository(database);
+const authenticationMiddleware = new AuthenticationMiddleware(
+  settingsRepository
+);
+
+// get all units without pagination endpoint
+router.get(PATH.UNITS, unitController.getAllUnits);
+
+// get units by `unit_sku` endpoint.
+router.get(
+  PATH.UNITS_SKU,
+  getUnitsBySkuQueryParams,
+  authenticationMiddleware.BasicAuthVerifier(),
+  unitController.getAllUnitsBySKU
+);
+
+// get sku's
+router.get(
+  PATH.SKU,
+  authenticationMiddleware.BasicAuthVerifier(),
+  unitController.getSKU
+);
 
 export default router;
