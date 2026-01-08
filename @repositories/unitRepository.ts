9be3af3d930abff6df9b368465bd21af3b59d703
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-08 23:58:24 +0800

    Add getByUnitSku async method for retrieving unit details using `unit_sku`

--- @repositories/unitRepository.ts
+++ @repositories/unitRepository.ts
@@ -1,6 +1,7 @@
 import { Database } from "../@config";
 import { Pool } from "mysql2";
 import { UnitsQueryResults } from "./../@types";
+import { RowDataPacket } from "mysql2";
 
 export class UnitRepository {
   private query: Pool;
@@ -24,6 +25,34 @@ export class UnitRepository {
   }
 
   async getAllWithPagination(limit: number, offset: number) {}
+
+  async getByUnitSKU(item_sku: string): Promise<UnitsQueryResults[]> {
+    const sql = "SELECT id, unit_name FROM units WHERE unit_sku = ?";
+
+    return new Promise((resolve, reject) => {
+      this.query.query(sql, [item_sku], (err, results: UnitsQueryResults[]) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(results);
+      });
+    });
+  }
+
+  async getBySKU(): Promise<UnitsQueryResults[]> {
+    const sql = "SELECT DISTINCT unit_sku FROM units ORDER BY unit_sku";
+
+    return new Promise((resolve, reject) => {
+      this.query.query(sql, (err, results: UnitsQueryResults[]) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(results);
+      });
+    });
+  }
 
   async getById() {}
 
