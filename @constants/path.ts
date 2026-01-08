44bc917c8f3b81ba53fa4df303c2c131c10d1063
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-08 23:56:33 +0800

    ADD UNITS & SKU path constant

--- @constants/path.ts
+++ @constants/path.ts
@@ -4,6 +4,8 @@ export const PATH = {
   EDIT_REPAIR_ID: "/edit/repair/:id",
   REPAIRS_REPAIRED: "/repairs/repaired",
   UNITS: "/units",
+  UNITS_SKU: "/units/sku",
   UNIT_ID: "/unit/:id",
   EDIT_UNIT_ID: "/edit/unit/:id",
+  SKU: "/sku",
 };
