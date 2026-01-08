af5cd473b71774241e10df9c7cdae13e918b9fa3
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-08 23:57:38 +0800

    Add `getAllUnitsBySKU` & `getSKU` request response layer

--- @controllers/unitController.ts
+++ @controllers/unitController.ts
@@ -1,7 +1,7 @@
 import { STATUSCODE } from "../@constants";
 import { UnitService } from "../@services/unitService";
 import { MiddlewareFn } from "../@types";
-import { logger, SuccessHandler } from "../@utils";
+import { logger, SuccessHandler, expressValidationResults } from "../@utils";
 
 export class UnitController {
   constructor(private unitService: UnitService) {}
@@ -17,6 +17,68 @@ export class UnitController {
 
     logger.info({
       GET_ALL_UNITS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, results, "Success");
+  };
+
+  getAllUnitsBySKU: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_UNITS_BY_SKU_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    expressValidationResults(req);
+
+    const results = await this.unitService.getUnitNamesByUnitSKU(
+      req.query.unit_sku as string
+    );
+
+    logger.info({
+      GET_ALL_UNITS_BY_SKU_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, results, "Success");
+  };
+
+  getUnitSKU: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_UNIT_SKU_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    expressValidationResults(req);
+
+    const results = await this.unitService.getUnitNamesByUnitSKU(
+      req.query.unit_sku as string
+    );
+
+    logger.info({
+      GET_UNIT_SKU_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, results, "Success");
+  };
+
+  getSKU: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_SKU_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const results = await this.unitService.getUnitSKU();
+
+    logger.info({
+      GET_SKU_RESPONSE: {
         message: "SUCCESS",
       },
     });
